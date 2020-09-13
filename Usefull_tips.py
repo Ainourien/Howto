4e17e25a3a0efc642e8d7c85a7761e560f77eb64
Ainourien <59263994+Ainourien@users.noreply.github.com>
2020-09-13 21:57:40 +0300

    Update Usefull_tips.py

--- Usefull_tips.py
+++ Usefull_tips.py
@@ -13,3 +13,20 @@
 # Записывает массив в директорию над текущей 
     with open ('..\\0101\\Source.txt', 'w') as f:
         f.write(''.join(ATCG))
+        
+# Try/Except:
+dirs = ['..\\0101\\Binary_Low_A\\', '..\\0101\\Binary_Low_T\\', '..\\0101\\Binary_Low_C\\', '..\\0101\\Binary_Low_G\\']
+for i in range(len(dirs)):
+    list_of_files = os.listdir(dirs[i])
+    try:
+        for x in range(len(list_of_files)):
+            #Перебираем элементы списка и оставляем только те, названия которых заканчиваются на .txt
+            ext = re.findall(r'.\w+', list_of_files[x])          
+            ext = ext[-1]        
+            if ext != '.txt':
+                del list_of_files[x]
+        #try/except нужен потому что после удаления элемента списка, количество элементов меняется, а от него зависит количество проходов цикла.
+        for j in range(len(list_of_files)):
+            zeros_count(dirs[i], str(list_of_files[j])) # Запускаем процедуру, которая создает папку, подсчитывает нули между единицами для каждого файла
+    except IndexError:
+        pass
